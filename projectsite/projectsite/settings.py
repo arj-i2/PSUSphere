85bf4c531ae7bbbc836649f3fdf3bce4ee5fc288
arj-i2 <senarj113@gmail.com>
2026-09-01 23:16:39 +0800

    fixed base

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -38,10 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
     'studentorg',
     
     'widget_tweaks',
+]
+
+SITE_ID = 2
+
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +62,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -142,3 +155,18 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login/'                  # where @login_required will send users
+LOGIN_REDIRECT_URL = '/'                        # where to go after successful login
+LOGOUT_REDIRECT_URL = '/accounts/login/'        # after logout, go back to login
+ACCOUNT_LOGOUT_REDIRECT_URL = '/'               # where to redirect after logout
+ACCOUNT_LOGOUT_ON_GET = True                    # logout immediately on GET
+ACCOUNT_LOGIN_METHODS = {"username", "email"}   # allow login with username OR email
+ACCOUNT_SIGNUP_FIELDS = [
+"username*",
+"email*",
+"password1*",
+"password2*",
+]
